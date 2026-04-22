522b26405834cbac2a3958d1f3d649642c32bae9
nelsonamo21-del <nelsonamo21@gmail.com>
2026-04-22 15:29:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function show_profile() {
+    document.getElementById("Info").innerHTML =
+    "Name: Nelson Amoako Yeboah. <br>  Course: IT <br> School: University of Ghana";
+}
+
+function show_picture(){
+    document.getElementById("pic").src = "WhatsApp Image 2026-04-03 at 20.27.01.jpeg";
+}
